39713cc9d660d40fea4bd9b851863fa8f6d9d931
Oneknowall <acalvin2@kent.edu>
2022-03-15 16:39:44 -0400

    Added new

---
+++ server/getNews.js
@@ -0,0 +1,14 @@
+const axios = require("axios").default
+
+async function getNews(){
+    await Promise.all([getOfficalTCGPokemon()])
+}
+
+async function getOfficalTCGPokemon(){
+    let response = await axios.get("https://www.pokemon.com/us/api/news/tcg-strategy?index=0&count=10")
+    console.log(response.data)
+}
+
+module.exports={
+    getNews
+}
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const getNews=require('./getNews.js').getNews
 const app=express();
 const path=require('path');
 const port=process.env.PORT || 3000;
@@ -7,4 +8,5 @@ app.use('/',express.static(path.join(__dirname,'compiled')));
 app.listen(port,()=>{
     console.log(__dirname)
     console.log('App is listening')
+    getNews()
 })
