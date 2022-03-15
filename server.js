f277330e08495691a26b7a0f20e828f7e541a1e2
JulianKQuesada <juliankquesada@gmail.com>
2022-03-15 13:37:00 -0400

    Changed structure for heroku deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const port=process.env.port || 3000;
-app.use('/',express.static(path.join(__dirname,'pokenews')));
+app.use('/',express.static(path.join(__dirname,'compiled')));
 
 app.listen(port,()=>{
     console.log(__dirname)
